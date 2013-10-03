40ef98b4dbfc2f749916e2674e59178b2d0e1126
Andy Fingerhut <andy_fingerhut@alum.wustl.edu>
2013-10-03 00:26:27 -0700

    Add tests for expected behavior of other set implementations

--- project.clj
+++ project.clj
@@ -1,6 +1,8 @@
 (defproject funjible "0.1.0-SNAPSHOT"
   :description "Almost, but not quite, exactly like Clojure core libraries"
-  :url "http://github.com/jafingerhut/fungible"
+  :url "http://github.com/jafingerhut/funjible"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :profiles {:dev {:dependencies [[avl.clj "0.0.8"]
+                                  [immutable-bitset "0.1.3"]]}})
--- test/funjible/set_test.clj
+++ test/funjible/set_test.clj
@@ -1,6 +1,31 @@
 (ns funjible.set-test
   (:require [clojure.test :refer :all]
-            [funjible.set :as f]))
+            [funjible.set :as f]
+            [avl.clj :as avl]
+            [immutable-bitset :as bitset]))
+
+
+(deftest they-are-all-sets
+  (let [sets-with-transient-impl [(hash-set 1 2 3)
+                                  (set [1 2 3])
+                                  (avl/sorted-set 1 2 3)
+                                  (avl/sorted-set-by > 1 2 3)
+                                  (bitset/sparse-bitset [1 2 3])
+                                  (bitset/dense-bitset [1 2 3])]
+        sets-without-transient-impl [(sorted-set 1 2 3)
+                                     (sorted-set-by > 1 2 3)]
+        sets-all (concat sets-with-transient-impl sets-without-transient-impl)]
+    (doseq [s sets-all]
+      (is (set? s)))
+    (doseq [s1 sets-all]
+      (doseq [s2 sets-all]
+        (is (= s1 s2))))
+    (doseq [s sets-with-transient-impl]
+      (is (instance? clojure.lang.IEditableCollection s))
+      (is (= s (persistent! (transient s)))))
+    (doseq [s sets-without-transient-impl]
+      (is (thrown? ClassCastException (transient s))))))
+
 
 (deftest throw-on-non-set-args
   (is (thrown? AssertionError (f/union [1 2])))
