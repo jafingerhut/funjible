95021817b75c09c75d75ea0b182e7ebedc3657a1
Andy Fingerhut <andy_fingerhut@alum.wustl.edu>
2013-10-08 02:18:40 -0700

    Prepare for release of version 0.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject funjible "0.1.0-SNAPSHOT"
+(defproject funjible "0.1.0"
   :description "Almost, but not quite, exactly like Clojure core libraries"
   :url "http://github.com/jafingerhut/funjible"
   :license {:name "Eclipse Public License"
