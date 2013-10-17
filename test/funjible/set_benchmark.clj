2cb849703186cacd40f11f5a79198f2c18a43b1b
Andy Fingerhut <andy_fingerhut@alum.wustl.edu>
2013-10-16 19:00:46 -0700

    Add benchmarks for fn difference

--- test/funjible/set_benchmark.clj
+++ test/funjible/set_benchmark.clj
@@ -7,6 +7,7 @@
             [clojure.pprint :as pp]
             [funjible.set :as fset]
             [funjible.set-precondition-mods-only :as fset-pre-only]
+            [funjible.set-precondition-always-transient-mods :as fset-trans]
             [clojure.set :as cset]
             [avl.clj :as avl]
             [immutable-bitset :as bitset]
@@ -149,6 +150,11 @@
         (recur (read rdr false :eof))))))
 
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;; Benchmarks for: union
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;; Main factors in performance of union function with two sets as
 ;; args:
@@ -217,15 +223,18 @@
             [(range 0 1000) (range 1000 2000)]
             ]]
     (let [s1 (f seq1), s2 (f seq2)]
-      ;;(printf "\n===== %s (type s1)=%s\n\n" desc (type s1))
-      ;;(println "--- (clojure.set/union" s1 s2 ")")
       (benchmark {:fn "clojure.set/union" :set-type desc :args [s1 s2]}
                  [] (cset/union s1 s2))
-      ;;(println "--- (funjible.set/union" s1 s2 ")")
-      (benchmark {:fn "funjible.set/union" :set-type desc :args [s1 s2]}
-                 [] (fset/union s1 s2))
+      (benchmark {:fn "funjible.set-trans/union" :set-type desc :args [s1 s2]}
+                 [] (fset-trans/union s1 s2))
       )))
 
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;; Benchmarks for: intersection
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;; intersection is implemented similarly to union, except the smaller
 ;; of two argument sets is used as the 'base' set, and the larger one
@@ -285,14 +294,116 @@
             [(range 0 1000) (range 1000 3000)]
             ]]
     (let [s1 (f seq1), s2 (f seq2)]
-      ;;(iprintf *err* "\n===== %s (type s1)=%s\n\n" desc (type s1))
-      ;;(iprintf *err* "--- (clojure.set/intersection %s %s)" s1 s2)
       (benchmark {:fn "clojure.set/intersection" :set-type desc :args [s1 s2]}
                  [] (cset/intersection s1 s2))
-      ;;(iprintf *err* "--- (funjible.set/intersection %s %s)" s1 s2)
-      (benchmark {:fn "funjible.set/intersection" :set-type desc :args [s1 s2]}
-                 [] (fset/intersection s1 s2))
+      (benchmark {:fn "funjible.set-trans/intersection" :set-type desc :args [s1 s2]}
+                 [] (fset-trans/intersection s1 s2))
       )))
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;; Benchmarks for: difference
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; difference on two sets is implemented differently depending upon
+;; the relative sizes of the first and second set.
+
+;; If the first set is smaller, it iterates over its elements,
+;; checking whether each one is also in the second set, removing with
+;; disj or disj! the elements that are in the second set.
+
+;; If the first set is the same size or larger, it iterates over
+;; elements of the second set, doing disj or disj! on the first set
+;; for each such element.
+
+;; Performance tests to hit each of these cases well should include
+;; tests where:
+
+;; + the first set is smaller, and disjoint from the second set (no
+;; disj ops done).
+
+;; + the first set is smaller, and a subset of the second set (lots of
+;; disj ops that change the set).
+
+;; + the first set is larger, and disjoint from second set (all disj
+;; ops leave set unchanged).
+
+;; + the first set is larger, and a superset of the the second set
+;; (all disj ops change the set).
+
+(deftest ^:benchmark benchmark-difference-funjible.set-vs-clojure.set
+  (iprintf *err* "\nfunjible.set/difference vs. cojure.set/difference\n")
+  (doseq [ [f desc] [
+                     [#(apply hash-set %) "clojure.core/hash-set"]
+                     [#(apply sorted-set %) "clojure.core/sorted-set"]
+                     [#(apply avl/sorted-set %) "avl.clj/sorted-set"]
+                     [bitset/sparse-bitset "immutable-bitset/sparse-bitset"]
+                     [bitset/dense-bitset "immutable-bitset/dense-bitset"]
+                     ]
+           [seq1 seq2]
+           [
+            [[] []]
+            
+            ;; 1st is little bit smaller, disjoint
+            [(range 0    2) (range 1000 1010)]
+            [(range 0    3) (range 1000 1013)]
+            [(range 0    4) (range 1000 1014)]
+            [(range 0   20) (range 1000 1030)]
+            [(range 0 1000) (range 1000 2010)]
+
+            ;; 1st is a lot smaller, disjoint
+            [(range 0    2) (range 1000 4000)]
+            [(range 0    3) (range 1000 4000)]
+            [(range 0    4) (range 1000 4000)]
+            [(range 0   20) (range 1000 4000)]
+            [(range 0 1000) (range 1000 4000)]
+
+            ;; 1st is little bit smaller, subset of 2nd
+            [(range 0    2) (range 0    3)]
+            [(range 0    3) (range 0    4)]
+            [(range 0    4) (range 0    5)]
+            [(range 0   20) (range 0   21)]
+            [(range 0 1000) (range 0 1001)]
+
+            ;; 1st is a lot smaller, subset of 2nd
+            [(range 0    2) (range 0 3000)]
+            [(range 0    3) (range 0 3000)]
+            [(range 0    4) (range 0 3000)]
+            [(range 0   20) (range 0 3000)]
+            [(range 0 1000) (range 0 3000)]
+
+            ;; 1st is little bit larger, disjoint
+            [(range 0    2) (range 1000 1001)]
+            [(range 0    3) (range 1000 1002)]
+            [(range 0    4) (range 1000 1003)]
+            [(range 0   20) (range 1000 1019)]
+            [(range 0 1000) (range 1000 1999)]
+
+            ;; 1st is a lot larger, disjoint
+            [(range 0 3000) (range 4000 4003)]
+            [(range 0 3000) (range 4000 5000)]
+
+            ;; 1st is little bit larger, superset of 2nd
+            [(range 0    2) (range 0    1)]
+            [(range 0    3) (range 0    2)]
+            [(range 0    4) (range 0    3)]
+            [(range 0   20) (range 0   19)]
+            [(range 0 1000) (range 0  999)]
+
+            ;; 1st is a lot larger, superset of 2nd
+            [(range 0 1000) (range 0    3)]
+            [(range 0 3000) (range 0    3)]
+            [(range 0 3000) (range 0 1000)]
+            ]]
+    (let [s1 (f seq1), s2 (f seq2)]
+      (benchmark {:fn "clojure.set/difference" :set-type desc :args [s1 s2]}
+                 [] (cset/difference s1 s2))
+      (benchmark {:fn "funjible.set-trans/difference" :set-type desc :args [s1 s2]}
+                 [] (fset-trans/difference s1 s2))
+      )))
+
 
 
 (comment
